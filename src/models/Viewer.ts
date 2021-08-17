e6049ddb8552f63e558c920bcf2373c205be7796
Daniel Büchele <daniel@buechele.cc>
2021-08-18 00:36:49 +0100

    viewer id

--- src/models/Viewer.ts
+++ src/models/Viewer.ts
@@ -1,9 +1,12 @@
 import {objectType} from 'nexus';
 import {Viewer} from 'nexus-prisma';
+import Node from './Node';
 
 export default objectType({
   name: 'Viewer',
   definition(t) {
+    t.field(Viewer.id);
+    t.implements(Node);
     t.field(Viewer.displayName);
     t.field(Viewer.email);
     t.field(Viewer.profilePicture);
