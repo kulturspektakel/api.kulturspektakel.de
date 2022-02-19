37bb42a846ae2c3dfe929a14a85aa5bc4c1c2132
Daniel Büchele <daniel@buechele.cc>
2022-02-19 21:33:08 +0000

    deploy?

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,5 +3,6 @@ yarn install --production
 yarn generate:prisma
 yarn dotenv-cli pull production
 yarn dotenv-cli pull
+# Merging production env with dev env to override values
 cat .env.production >> .env
 supervisorctl restart api
