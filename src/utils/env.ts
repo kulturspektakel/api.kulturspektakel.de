eb12fa4183b865271defc76180798268c73d8434
Daniel Büchele <daniel@buechele.cc>
2022-04-22 22:22:44 +0100

    required

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,28 +1,41 @@
 import env from 'env-var';
 
 export default {
-  NODE_ENV: env.get('NODE_ENV').asEnum(['development', 'production', 'test']),
-  PORT: env.get('PORT').asIntPositive(),
-  DATABASE_URL: env.get('DATABASE_URL').asString(),
-  JWT_SECRET: env.get('JWT_SECRET').asString(),
-  SLACK_CLIENT_ID: env.get('SLACK_CLIENT_ID').asString(),
-  SLACK_CLIENT_SECRET: env.get('SLACK_CLIENT_SECRET').asString(),
-  SLACK_TEAM_ID: env.get('SLACK_TEAM_ID').asString(),
-  PASSKIT_KEY: env.get('PASSKIT_KEY').asString(),
-  AWS_ACCESS_KEY_ID: env.get('AWS_ACCESS_KEY_ID').asString(),
-  AWS_SECRET_ACCESS_KEY: env.get('AWS_SECRET_ACCESS_KEY').asString(),
-  KULT_WEBSITE_API_PASSWORD: env.get('KULT_WEBSITE_API_PASSWORD').asString(),
-  KULT_WEBSITE_API_EMAIL: env.get('KULT_WEBSITE_API_EMAIL').asString(),
-  SLACK_BOT_TOKEN: env.get('SLACK_BOT_TOKEN').asString(),
-  KULT_CASH_SALT: env.get('KULT_CASH_SALT').asString(),
-  BING_MAPS_KEY: env.get('BING_MAPS_KEY').asString(),
-  NUCLINO_WORKSPACE_ID: env.get('NUCLINO_WORKSPACE_ID').asString(),
-  NUCLINO_TEAM_ID: env.get('NUCLINO_TEAM_ID').asString(),
-  NUCLINO_API_KEY: env.get('NUCLINO_API_KEY').asString(),
-  NUCLINO_ANONYMOUS_PASSWORD: env.get('NUCLINO_ANONYMOUS_PASSWORD').asString(),
+  NODE_ENV: env
+    .get('NODE_ENV')
+    .required()
+    .asEnum(['development', 'production', 'test']),
+  PORT: env.get('PORT').required().asIntPositive(),
+  DATABASE_URL: env.get('DATABASE_URL').required().asString(),
+  JWT_SECRET: env.get('JWT_SECRET').required().asString(),
+  SLACK_CLIENT_ID: env.get('SLACK_CLIENT_ID').required().asString(),
+  SLACK_CLIENT_SECRET: env.get('SLACK_CLIENT_SECRET').required().asString(),
+  SLACK_TEAM_ID: env.get('SLACK_TEAM_ID').required().asString(),
+  PASSKIT_KEY: env.get('PASSKIT_KEY').required().asString(),
+  AWS_ACCESS_KEY_ID: env.get('AWS_ACCESS_KEY_ID').required().asString(),
+  AWS_SECRET_ACCESS_KEY: env.get('AWS_SECRET_ACCESS_KEY').required().asString(),
+  KULT_WEBSITE_API_PASSWORD: env
+    .get('KULT_WEBSITE_API_PASSWORD')
+    .required()
+    .asString(),
+  KULT_WEBSITE_API_EMAIL: env
+    .get('KULT_WEBSITE_API_EMAIL')
+    .required()
+    .asString(),
+  SLACK_BOT_TOKEN: env.get('SLACK_BOT_TOKEN').required().asString(),
+  KULT_CASH_SALT: env.get('KULT_CASH_SALT').required().asString(),
+  BING_MAPS_KEY: env.get('BING_MAPS_KEY').required().asString(),
+  NUCLINO_WORKSPACE_ID: env.get('NUCLINO_WORKSPACE_ID').required().asString(),
+  NUCLINO_TEAM_ID: env.get('NUCLINO_TEAM_ID').required().asString(),
+  NUCLINO_API_KEY: env.get('NUCLINO_API_KEY').required().asString(),
+  NUCLINO_ANONYMOUS_PASSWORD: env
+    .get('NUCLINO_ANONYMOUS_PASSWORD')
+    .required()
+    .asString(),
   GOOGLE_APPLICATION_CREDENTIALS: env
     .get('GOOGLE_APPLICATION_CREDENTIALS')
+    .required()
     .asString(),
-  SAML_CERT: env.get('SAML_CERT').asString(),
-  SAML_PRIVATE_KEY: env.get('SAML_PRIVATE_KEY').asString(),
+  SAML_CERT: env.get('SAML_CERT').required().asString(),
+  SAML_PRIVATE_KEY: env.get('SAML_PRIVATE_KEY').required().asString(),
 };
