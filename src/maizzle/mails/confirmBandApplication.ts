b8cf732d1a28ac7facb34bbd962dc351ecc72f39
Daniel Büchele <daniel@buechele.cc>
2022-09-04 16:26:36 +0100

    maizzle 4

--- src/maizzle/mails/confirmBandApplication.ts
+++ src/maizzle/mails/confirmBandApplication.ts
@@ -1,9 +1,9 @@
 // auto-generated file using yarn generate:mail
 // prettier-ignore
-export default ({bandname, eventYear}: {bandname: string, eventYear: string}) => `<!DOCTYPE {{{ page.doctype || 'html' }}}>
-<html lang="{{ page.language || 'de' }}" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+export default ({bandname, eventYear, $$typeof}: {bandname: string, eventYear: string, $$typeof: string}) => `<!DOCTYPE html>
+<html lang="de" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
-  <meta charset="{{ page.charset || 'utf-8' }}">
+  <meta charset="utf-8">
   <meta name="x-apple-disable-message-reformatting">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,49 +13,56 @@ export default ({bandname, eventYear}: {bandname: string, eventYear: string}) =>
     <style>
       td,th,div,p,a,h1,h2,h3,h4,h5,h6 {font-family: "Segoe UI", sans-serif; mso-line-height-rule: exactly;}
     </style>
-  <![endif]-->
-  <if condition="page.title">
-    <title>{{{ page.title }}}</title>
-  </if>
-  <if condition="page.googleFonts">
-    <link href="https://api.kulturspektakel.de/public/maizzle/https://fonts.googleapis.com/css?family={{ page.googleFonts }}" rel="stylesheet" media="screen">
-  </if>
-  <if condition="page.css"> </if></head>
-<body class="page_bodyClass">
-  <if condition="page.preheader">
-    <div>{{{ page.preheader }}}&#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &zwnj;
+  <![endif]-->    <title>Bewerbung „undefined“ beim Kulturspektakel undefined</title>    <style>.hover-underline:hover {
+  text-decoration-line: underline !important;
+}
+@media (max-width: 600px) {
+  .sm-w-full {
+    width: 100% !important;
+  }
+  .sm-p-24 {
+    padding: 96px !important;
+  }
+  .sm-px-24 {
+    padding-left: 96px !important;
+    padding-right: 96px !important;
+  }
+  .sm-leading-32 {
+    line-height: 128px !important;
+  }
+}</style></head>
+<body style="margin: 0; width: 100%; padding: 0; word-break: break-word; -webkit-font-smoothing: antialiased; background-color: #f3f4f6">    <div style="display: none">Eure Bewerbung mit „undefined“ für das Kulturspektakel Gauting undefined&#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &zwnj;
       &#160;&#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &zwnj;
       &#160;&#847; &#847; &#847; &#847; &#847; </div>
-  </if>
-  <div role="article" aria-roledescription="email" aria-label="{{{ page.title || '' }}}" lang="{{ page.language || 'de' }}">
-    <table cellpadding="0" cellspacing="0" role="presentation">
+  <div role="article" aria-roledescription="email" aria-label="Bewerbung „undefined“ beim Kulturspektakel undefined" lang="de">
+    <table style="width: 100%; font-family: ui-sans-serif, system-ui, -apple-system, 'Segoe UI', sans-serif" cellpadding="0" cellspacing="0" role="presentation">
       <tr>
-        <td align="center">
-          <table cellpadding="0" cellspacing="0" role="presentation">
+        <td align="center" style="background-color: #f3f4f6">
+          <table class="sm-w-full" cellpadding="0" cellspacing="0" role="presentation">
             <tr>
-              <td></td>
+              <td style="height: 96px"></td>
             </tr>
             <tr>
-              <td align="center">
-                <table cellpadding="0" cellspacing="0" role="presentation">
+              <td align="center" class="sm-px-24">
+                <table style="width: 100%" cellpadding="0" cellspacing="0" role="presentation">
                   <tr>
-                    <td>    <h1>Bewerbung für das Kulturspektakel ${eventYear}</h1>
+                    <td class="sm-p-24" style="border-radius: 4px; background-color: #fff; padding: 192px; text-align: left; font-size: 16px; line-height: 96px; color: #1f2937">    <h1 class="sm-leading-32" style="margin: 0; margin-bottom: 96px; font-size: 24px; font-weight: 600; color: #000">Bewerbung für das Kulturspektakel ${eventYear}</h1>
     <p>Vielen Dank für eure Bewerbung mit „${bandname}“ für das Kulturspektakel Gauting ${eventYear}. Jede Bewerbung bekommt von uns eine Zu- oder Absage. Es kann allerdings bis wenige Monate vor dem Kulturspektakel dauern, bis ihr eine Antwort bekommt. Daher bitten wir euch gedulig zu sein.</p>
-    <p>Viele Grüße,<br>Das Kulturspektakel-Booking-Team</p>                    </td>
+    <p style="margin: 0">Viele Grüße,<br>Das Kulturspektakel-Booking-Team</p>                    </td>
                   </tr>
                   <tr>
-                    <td></td>
+                    <td style="height: 144px"></td>
                   </tr>
                   <tr>
-                    <td>
-                      <img src="https://api.kulturspektakel.de/public/maizzle/logo.png" width="40" alt="Kulturspektakel Gauting">
-                      <p>Kulturspektakel Gauting e.V.</p>
-                      <p>Anschrift: Bahnhofstr. 6, 82131 Gauting</p>
-                      <p>Festivalgelände: Germeringer Str. 41, Gauting</p>
-                      <p>
-                        <a href="https://kulturspektakel.de">Webseite</a> &bull;
-                        <a href="https://facebook.com/kulturspektakel">Facebook</a> &bull;
-                        <a href="https://instagram.com/kulturspektakel">Instagram</a>
+                    <td style="padding-left: 96px; padding-right: 96px; text-align: center; font-size: 12px; color: #4b5563">
+                      <img src="https://api.kulturspektakel.de/public/maizzle/logo.png" width="40" alt="Kulturspektakel Gauting" style="border: 0; max-width: 100%; vertical-align: middle; line-height: 100%">
+                      <p style="margin: 0; margin-top: 24px; margin-bottom: 16px; font-weight: 600">Kulturspektakel Gauting e.V.</p>
+                      <p style="margin: 0; font-style: italic">Anschrift: Bahnhofstr. 6, 82131 Gauting</p>
+                      <p style="margin: 0; font-style: italic">Festivalgelände: Germeringer Str. 41, Gauting</p>
+                      <p style="cursor: default">
+                        <a href="https://kulturspektakel.de" class="hover-underline" style="text-decoration-line: none">Webseite</a> &bull;
+                        <a href="https://facebook.com/kulturspektakel" class="hover-underline" style="text-decoration-line: none">Facebook</a> &bull;
+                        <a href="https://instagram.com/kulturspektakel" class="hover-underline" style="text-decoration-line: none">Instagram</a>
                       </p>
                     </td>
                   </tr>
--- src/maizzle/mails/rejectBandApplication.ts
+++ src/maizzle/mails/rejectBandApplication.ts
@@ -1,9 +1,9 @@
 // auto-generated file using yarn generate:mail
 // prettier-ignore
-export default ({bandname, eventYear}: {bandname: string, eventYear: string}) => `<!DOCTYPE {{{ page.doctype || 'html' }}}>
-<html lang="{{ page.language || 'de' }}" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+export default ({bandname, eventYear, $$typeof}: {bandname: string, eventYear: string, $$typeof: string}) => `<!DOCTYPE html>
+<html lang="de" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
 <head>
-  <meta charset="{{ page.charset || 'utf-8' }}">
+  <meta charset="utf-8">
   <meta name="x-apple-disable-message-reformatting">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,33 +13,43 @@ export default ({bandname, eventYear}: {bandname: string, eventYear: string}) =>
     <style>
       td,th,div,p,a,h1,h2,h3,h4,h5,h6 {font-family: "Segoe UI", sans-serif; mso-line-height-rule: exactly;}
     </style>
-  <![endif]-->
-  <if condition="page.title">
-    <title>{{{ page.title }}}</title>
-  </if>
-  <if condition="page.googleFonts">
-    <link href="https://api.kulturspektakel.de/public/maizzle/https://fonts.googleapis.com/css?family={{ page.googleFonts }}" rel="stylesheet" media="screen">
-  </if>
-  <if condition="page.css"> </if></head>
-<body class="page_bodyClass">
-  <if condition="page.preheader">
-    <div>{{{ page.preheader }}}&#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &zwnj;
+  <![endif]-->    <title>Absage „undefined“ - Kulturspektakel undefined</title>    <style>.hover-underline:hover {
+  text-decoration-line: underline !important;
+}
+.hover-no-underline:hover {
+  text-decoration-line: none !important;
+}
+@media (max-width: 600px) {
+  .sm-w-full {
+    width: 100% !important;
+  }
+  .sm-p-24 {
+    padding: 96px !important;
+  }
+  .sm-px-24 {
+    padding-left: 96px !important;
+    padding-right: 96px !important;
+  }
+  .sm-leading-32 {
+    line-height: 128px !important;
+  }
+}</style></head>
+<body style="margin: 0; width: 100%; padding: 0; word-break: break-word; -webkit-font-smoothing: antialiased; background-color: #f3f4f6">    <div style="display: none">Absage zu eurer Bewerbung mit „undefined“ für das Kulturspektakel Gauting undefined&#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &zwnj;
       &#160;&#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &#847; &zwnj;
       &#160;&#847; &#847; &#847; &#847; &#847; </div>
-  </if>
-  <div role="article" aria-roledescription="email" aria-label="{{{ page.title || '' }}}" lang="{{ page.language || 'de' }}">
-    <table cellpadding="0" cellspacing="0" role="presentation">
+  <div role="article" aria-roledescription="email" aria-label="Absage „undefined“ - Kulturspektakel undefined" lang="de">
+    <table style="width: 100%; font-family: ui-sans-serif, system-ui, -apple-system, 'Segoe UI', sans-serif" cellpadding="0" cellspacing="0" role="presentation">
       <tr>
-        <td align="center">
-          <table cellpadding="0" cellspacing="0" role="presentation">
+        <td align="center" style="background-color: #f3f4f6">
+          <table class="sm-w-full" cellpadding="0" cellspacing="0" role="presentation">
             <tr>
-              <td></td>
+              <td style="height: 96px"></td>
             </tr>
             <tr>
-              <td align="center">
-                <table cellpadding="0" cellspacing="0" role="presentation">
+              <td align="center" class="sm-px-24">
+                <table style="width: 100%" cellpadding="0" cellspacing="0" role="presentation">
                   <tr>
-                    <td>    <h1>Absage „${bandname}“</h1>
+                    <td class="sm-p-24" style="border-radius: 4px; background-color: #fff; padding: 192px; text-align: left; font-size: 16px; line-height: 96px; color: #1f2937">    <h1 class="sm-leading-32" style="margin: 0; margin-bottom: 96px; font-size: 24px; font-weight: 600; color: #000">Absage „${bandname}“</h1>
     <p>
         Hey,
     </p>
@@ -56,27 +66,27 @@ export default ({bandname, eventYear}: {bandname: string, eventYear: string}) =>
         Dementsprechend würden wir uns freuen, wenn ihr euch auch nächstes Jahr wieder bei uns bewerbt. Die Bewerbung für das nächstjährige Kulturspektakel Jahr wird ab August über das Bewerbungsformular auf unserer Webseite möglich sein.
     </p>
     <p>
-        Das Line-Up für unser diesjähriges Festival findet ihr unter: <a href="https://kulturspektakel.de/lineup/${eventYear}">https://kulturspektakel.de/lineup/${eventYear}</a>
+        Das Line-Up für unser diesjähriges Festival findet ihr unter: <a href="https://kulturspektakel.de/lineup/${eventYear}" class="hover-no-underline" style="text-decoration-line: underline">https://kulturspektakel.de/lineup/${eventYear}</a>
 . Wir würden uns sehr freuen, wenn ihr trotzdem als Gäste beim Kulturspektakel vorbeischaut.
     </p>
-    <p>
+    <p style="margin: 0">
         Viel Erfolg und hoffentlich bis zum nächsten Jahr!<br>
         Das Bookingteam des Kulturspektakel Gauting
     </p>                    </td>
                   </tr>
                   <tr>
-                    <td></td>
+                    <td style="height: 144px"></td>
                   </tr>
                   <tr>
-                    <td>
-                      <img src="https://api.kulturspektakel.de/public/maizzle/logo.png" width="40" alt="Kulturspektakel Gauting">
-                      <p>Kulturspektakel Gauting e.V.</p>
-                      <p>Anschrift: Bahnhofstr. 6, 82131 Gauting</p>
-                      <p>Festivalgelände: Germeringer Str. 41, Gauting</p>
-                      <p>
-                        <a href="https://kulturspektakel.de">Webseite</a> &bull;
-                        <a href="https://facebook.com/kulturspektakel">Facebook</a> &bull;
-                        <a href="https://instagram.com/kulturspektakel">Instagram</a>
+                    <td style="padding-left: 96px; padding-right: 96px; text-align: center; font-size: 12px; color: #4b5563">
+                      <img src="https://api.kulturspektakel.de/public/maizzle/logo.png" width="40" alt="Kulturspektakel Gauting" style="border: 0; max-width: 100%; vertical-align: middle; line-height: 100%">
+                      <p style="margin: 0; margin-top: 24px; margin-bottom: 16px; font-weight: 600">Kulturspektakel Gauting e.V.</p>
+                      <p style="margin: 0; font-style: italic">Anschrift: Bahnhofstr. 6, 82131 Gauting</p>
+                      <p style="margin: 0; font-style: italic">Festivalgelände: Germeringer Str. 41, Gauting</p>
+                      <p style="cursor: default">
+                        <a href="https://kulturspektakel.de" class="hover-underline" style="text-decoration-line: none">Webseite</a> &bull;
+                        <a href="https://facebook.com/kulturspektakel" class="hover-underline" style="text-decoration-line: none">Facebook</a> &bull;
+                        <a href="https://instagram.com/kulturspektakel" class="hover-underline" style="text-decoration-line: none">Instagram</a>
                       </p>
                     </td>
                   </tr>
