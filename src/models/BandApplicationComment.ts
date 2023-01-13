42940657db657c54c216c52ce645f4c768034aad
Daniel Büchele <daniel@buechele.cc>
2023-01-13 21:52:47 +0000

    comment ID

--- src/models/BandApplicationComment.ts
+++ src/models/BandApplicationComment.ts
@@ -2,6 +2,7 @@ import {builder} from '../pothos/builder';
 
 export default builder.prismaObject('BandApplicationComment', {
   fields: (t) => ({
+    id: t.exposeID('id'),
     user: t.relation('viewer'),
     comment: t.exposeString('comment'),
     createdAt: t.expose('createdAt', {type: 'DateTime'}),
