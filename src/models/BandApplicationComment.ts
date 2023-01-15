eea0263e012e6f664c890e4e30bb2e9abc1324ec
Daniel Büchele <daniel@buechele.cc>
2023-01-15 16:36:44 +0000

    node

--- src/models/BandApplicationComment.ts
+++ src/models/BandApplicationComment.ts
@@ -1,8 +1,8 @@
 import {builder} from '../pothos/builder';
 
-export default builder.prismaObject('BandApplicationComment', {
+export default builder.prismaNode('BandApplicationComment', {
+  id: {field: 'id'},
   fields: (t) => ({
-    id: t.exposeID('id'),
     user: t.relation('viewer'),
     comment: t.exposeString('comment'),
     createdAt: t.expose('createdAt', {type: 'DateTime'}),
