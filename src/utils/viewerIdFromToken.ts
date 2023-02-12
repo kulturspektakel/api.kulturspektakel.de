1353fe792aad3a8fd6debb50e9aa1e46c15130fb
Daniel Büchele <daniel@buechele.cc>
2023-02-12 01:20:40 +0000

    tes

--- src/utils/viewerIdFromToken.ts
+++ src/utils/viewerIdFromToken.ts
@@ -9,7 +9,7 @@ export default async function viewerIdFromToken(
   } else if (parsedToken.iss === 'directus') {
     const user = await prismaClient.$queryRaw<{
       external_identifier: string;
-    } | null>`select "external_identifier" from "directus"."directus_users" where "id"='${parsedToken?.id}'`;
+    } | null>`select "external_identifier" from "directus"."directus_users" where "id"=${parsedToken.id}`;
     return user?.external_identifier;
   } else if (parsedToken.iss == null && parsedToken.type === 'user') {
     return parsedToken.userId;
