8c7fd02d756e5e4995e6fbd20525d3b718454941
Daniel Büchele <daniel@buechele.cc>
2023-03-14 22:45:13 +0000

    u

--- src/utils/sanitizeTSQuery.ts
+++ src/utils/sanitizeTSQuery.ts
@@ -1,8 +1,14 @@
 export default function sanitizeTSQuery(query: string): string {
+  console.log(
+    query
+      // sanitize tsquery: Only Letters, spaces, dash
+      .replace(/[^\p{L}0-9- ]/gu, ' '),
+    // remove spaces in front and beginning
+  );
   return (
     query
       // sanitize tsquery: Only Letters, spaces, dash
-      .replace(/[^\p{L}0-9- ]/g, ' ')
+      .replace(/[^\p{L}0-9- ]/gu, ' ')
       // remove spaces in front and beginning
       .trim()
       // sanitize tsquery: Only Letters, spaces, dash
