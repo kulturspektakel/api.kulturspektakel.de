89306cb4d432b0d8661543615086bd33ac99c37f
Daniel Büchele <daniel@buechele.cc>
2023-07-09 19:00:48 +0100

    pics

--- src/routes/slack/owntracks.ts
+++ src/routes/slack/owntracks.ts
@@ -2,7 +2,7 @@ import prismaClient from '../../utils/prismaClient';
 import express from 'express';
 import {Router} from '@awaitjs/express';
 import {SlackSlashCommandRequest} from './token';
-import {configUrl} from '../owntracks';
+import {configString} from '../owntracks';
 
 const router = Router({});
 
@@ -29,9 +29,9 @@ router.postAsync(
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `\n1. *OwnTracks* herunterladen: <https://apps.apple.com/de/app/owntracks/id692424691|iPhone> oder <https://play.google.com/store/apps/details?id=org.owntracks.android&hl=en&gl=US&pli=1|Android>\n2. Diesen ${configUrl(
+            text: `\n1. *OwnTracks* herunterladen: <https://apps.apple.com/de/app/owntracks/id692424691|iPhone> oder <https://play.google.com/store/apps/details?id=org.owntracks.android&hl=en&gl=US&pli=1|Android>\n2. Diesen <https://api.kulturspektakel.de/owntracks/config?config=${configString(
               viewer,
-            )} anklicken um die App zu konfigurieren`,
+            )}|Link> anklicken um die App zu konfigurieren`,
           },
         },
       ],
