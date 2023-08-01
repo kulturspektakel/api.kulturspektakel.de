7111b29ce75d07a61db772e6e91d55e9730691a4
Daniel Büchele <daniel@buechele.cc>
2023-08-01 23:44:20 +0100

    days

--- src/routes/slack/lagerschluessel.ts
+++ src/routes/slack/lagerschluessel.ts
@@ -4,7 +4,11 @@ import {SlackSlashCommandRequest} from './token';
 import fetch from 'node-fetch';
 import env from '../../utils/env';
 import prismaClient from '../../utils/prismaClient';
-import {differenceInMinutes} from 'date-fns';
+import {
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+} from 'date-fns';
 
 const router = Router({});
 
@@ -65,7 +69,7 @@ router.postAsync(
         },
       });
     const mapLink = `https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}`;
-    const image_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7C${latitude},${longitude}&key=${env.GOOGLE_MAPS_KEY}`;
+    const image_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=14&size=600x300&maptype=roadmap&markers=color:red%7C${latitude},${longitude}&key=${env.GOOGLE_MAPS_KEY}&language=de`;
 
     const apiRequest = await fetch(
       `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${env.GOOGLE_MAPS_KEY}`,
@@ -74,7 +78,6 @@ router.postAsync(
     const data = await apiRequest.json();
     const address =
       data.results.at(0)?.formatted_address ?? 'Unbekannte Position';
-    const minutes = differenceInMinutes(new Date(), timeStamp);
 
     res.status(200).json({
       text: 'Lagerschlüssel Position',
@@ -83,7 +86,9 @@ router.postAsync(
           type: 'section',
           text: {
             type: 'mrkdwn',
-            text: `Der Lagerschlüssel wurde zuletzt vor ${minutes} Minuten in <${mapLink}|${address}> gesehen.`,
+            text: `Der Lagerschlüssel wurde zuletzt vor ${ago(
+              timeStamp,
+            )} Minuten in <${mapLink}|${address}> gesehen.`,
           },
         },
         {
@@ -100,5 +105,18 @@ router.postAsync(
     });
   },
 );
+
+function ago(d: Date) {
+  const minutes = differenceInMinutes(new Date(), d);
+  if (minutes < 60) {
+    return `${minutes} Minuten`;
+  }
+  const hours = differenceInHours(new Date(), d);
+  if (hours < 24) {
+    return `${hours} Stunden`;
+  }
+  const days = differenceInDays(new Date(), d);
+  return `${days} Tage`;
+}
 
 export default router;
