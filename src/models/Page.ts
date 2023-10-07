d7440b3da4f7d96df2270a99eecef6edee7f8cb7
Veronika Fuchsberger <vronifuchsberger@googlemail.com>
2023-10-07 16:13:48 +0100

    page typefix

--- src/models/Page.ts
+++ src/models/Page.ts
@@ -3,10 +3,10 @@ import {builder} from '../pothos/builder';
 builder.prismaNode('Page', {
   id: {field: 'slug'},
   fields: (t) => ({
-    bottom: t.exposeString('bottom'),
-    content: t.exposeString('content'),
-    left: t.exposeString('left'),
-    right: t.exposeString('right'),
+    bottom: t.exposeString('bottom', {nullable: true}),
+    content: t.exposeString('content', {nullable: true}),
+    left: t.exposeString('left', {nullable: true}),
+    right: t.exposeString('right', {nullable: true}),
     title: t.exposeString('title'),
   }),
 });
