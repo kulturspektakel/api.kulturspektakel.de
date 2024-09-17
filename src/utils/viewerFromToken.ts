bcc04450ec130aa671f186791a6ec54d6d5c8e68
Daniel Büchele <daniel@buechele.cc>
2024-09-17 23:18:43 +0100

    caching, wow

--- src/utils/viewerFromToken.ts
+++ src/utils/viewerFromToken.ts
@@ -3,16 +3,23 @@ import {ParsedToken} from '../routes/auth';
 import prismaClient from './prismaClient';
 import viewerIdFromToken from './viewerIdFromToken';
 
+const CACHE = new Map<string, Promise<Viewer | null>>();
+
 export default async function viewerFromToken(
   parsedToken: ParsedToken | undefined,
 ): Promise<Viewer | null> {
   const viewerId = await viewerIdFromToken(parsedToken);
   if (viewerId) {
-    return await prismaClient.viewer.findUnique({
+    if (CACHE.has(viewerId)) {
+      return CACHE.get(viewerId) ?? null;
+    }
+    const promise = prismaClient.viewer.findUnique({
       where: {
         id: viewerId,
       },
     });
+    CACHE.set(viewerId, promise);
+    return promise;
   }
   return null;
 }
--- src/utils/viewerIdFromToken.ts
+++ src/utils/viewerIdFromToken.ts
@@ -1,18 +1,26 @@
 import {ParsedToken} from '../routes/auth';
 import prismaClient from './prismaClient';
 
+const CACHE = new Map<string, Promise<string | undefined>>();
+
 export default async function viewerIdFromToken(
   parsedToken: ParsedToken | undefined,
 ): Promise<string | undefined> {
   if (parsedToken == null) {
     return;
   } else if (parsedToken.iss === 'directus') {
-    const user = await prismaClient.$queryRaw<
+    if (CACHE.has(parsedToken.id)) {
+      return CACHE.get(parsedToken.id);
+    }
+    const promise = prismaClient.$queryRaw<
       Array<{
         external_identifier: string;
       }>
-    >`select "external_identifier" from "directus"."directus_users" where "id"=${parsedToken.id}::uuid`;
-    return user.pop()?.external_identifier;
+    >`select "external_identifier" from "directus"."directus_users" where "id"=${parsedToken.id}::uuid`.then(
+      (d) => d.pop()?.external_identifier,
+    );
+    CACHE.set(parsedToken.id, promise);
+    return promise;
   } else if (parsedToken.iss === 'owntracks') {
     return parsedToken.viewerId;
   }
