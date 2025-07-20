c1fd88fc436fb3d0fb3b82d6076921874ca1c69e
Daniel Büchele <dbuchele@figma.com>
2025-07-20 15:33:41 +0200

    env json

--- scripts/merge-envs.ts
+++ scripts/merge-envs.ts
@@ -1,24 +1,19 @@
 import path from 'path';
 import {promises as fs} from 'fs';
-import {parse} from 'dotenv';
+import {parse} from '@dotenvx/dotenvx';
 
-const ENV_PROD_PATH = path.join(__dirname, '..', '.env.production');
 const ENV_PATH = path.join(__dirname, '..', '.env');
 const ENV_YML = path.join(__dirname, '..', '.env.json');
 
 (async () => {
-  const dev = await readEnv(ENV_PATH);
-  const prod = await readEnv(ENV_PROD_PATH);
-  const config = {...dev, ...prod};
+  const config = await readEnv(ENV_PATH);
   delete config.PORT;
   await fs.writeFile(ENV_YML, JSON.stringify(config));
-  console.log('Merged configs into .env');
+  console.log('Created .env.json');
 })();
 
 async function readEnv(path: string) {
-  const config = await (
-    await fs.readFile(path)
-  )
+  const config = (await fs.readFile(path))
     .toString()
     .split('\n')
     // filter empty vars
